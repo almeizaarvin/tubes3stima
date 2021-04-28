f7a61dc801c5de33f943a7a73bb4483537bebb77
rafirm29 <rafiray.munandar@gmail.com>
2021-04-28 17:08:19 +0700

    Added contributors page

--- src/Bobobot.py
+++ src/Bobobot.py
@@ -3,14 +3,18 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.static_folder = 'static'
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route("/get")
+@app.route('/get')
 def get_bot_response():
     userText = request.args.get('msg')
     return userText
+
+@app.route('/contributors/')
+def contributors():
+    return render_template("contributors.html")
     
 if __name__ == "__main__":
     app.run(debug = True) 
